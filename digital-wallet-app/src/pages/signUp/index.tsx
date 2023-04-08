16556f1814c274914945effc4cd1e9f0397a26da
Rian Dantas Landim <rianlandim222@gmail.com>
2023-04-08 11:02:41 -0300

    added routing on user forms

--- digital-wallet-app/src/pages/signUp/index.tsx
+++ digital-wallet-app/src/pages/signUp/index.tsx
@@ -1,8 +1,11 @@
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { useForm } from 'react-hook-form'
 import { PersonalData } from './personalData'
 import zod from 'zod'
-import {zodResolver} from "@hookform/resolvers/zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { EmailPassword } from './emailPassword'
+import { MonthlyEarning } from './monthlyEarning'
+import { DayPayment } from './dayPayment'
 
 
 const zodSchema = zod.object({
@@ -13,18 +16,24 @@ const zodSchema = zod.object({
 type FormProps = zod.infer<typeof zodSchema>
 
 type StackNavigationScreens = {
-  personalData: undefined
+  personalData: undefined;
+  emailPassword: undefined;
+  montlyEarning: undefined;
+  dayPayment: undefined;
 }
 
 export function SignUpPage() {
 
   const Stack = createNativeStackNavigator<StackNavigationScreens>()
 
-  const {watch, handleSubmit} = useForm<FormProps>({resolver: zodResolver(zodSchema)})
+  const { watch, handleSubmit } = useForm<FormProps>({ resolver: zodResolver(zodSchema) })
 
   return (
-      <Stack.Navigator initialRouteName='personalData' screenOptions={{headerShown: false}}>
-        <Stack.Screen name='personalData' component={PersonalData} />
-      </Stack.Navigator>
+    <Stack.Navigator initialRouteName='personalData' screenOptions={{ headerShown: false }}>
+      <Stack.Screen name='personalData' component={PersonalData} />
+      <Stack.Screen name='emailPassword' component={EmailPassword} />
+      <Stack.Screen name='montlyEarning' component={MonthlyEarning} />
+      <Stack.Screen name='dayPayment' component={DayPayment} />
+    </Stack.Navigator>
   )
 }
