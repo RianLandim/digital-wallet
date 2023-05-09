ea656600e62c72d46f95df0e272a3f15793cc76d
Rian Dantas Landim <rianlandim222@gmail.com>
2023-05-09 12:05:55 -0300

    initiated route documentation with swagger

--- digital-wallet-api/src/infra/http/dtos/create-user-body.ts
+++ digital-wallet-api/src/infra/http/dtos/create-user-body.ts
@@ -1,21 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class CreateUserBody {
+  @ApiProperty()
   @IsNotEmpty()
   username: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   cpf: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   earning: number;
 
+  @ApiProperty()
   @IsNotEmpty()
   earningDay: number;
 }
--- digital-wallet-api/src/main.ts
+++ digital-wallet-api/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Carteira Digital Api')
+    .setDescription('')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3333);
